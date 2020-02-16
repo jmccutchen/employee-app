5dc5cd585bc30448db5c7e0a3aecab161ff12668
jmccutchen <purplegrits@yahoo.com>
2020-02-16 18:10:13 -0500

    updated README and added style

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # employee-app
+
+A mock employee listing that is sortable and searchable.  Built with React.
--- src/components/PageHeader/index.js
+++ src/components/PageHeader/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class PageHeader extends React.Component {
     render() {
         return(
-            <h1 className="jumbotron text-center">
+            <h1 className="jumbotron text-center bg-info text-white">
             The Greatest Employee App Evah
             </h1>
         );
